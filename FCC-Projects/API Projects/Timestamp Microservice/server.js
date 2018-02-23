9f9a8412ad16916801c26e8d3a468239af1f9334
tathagata218 <tathagata218@yahoo.com>
2018-02-22 23:34:35 -0600

    this is modified

--- FCC-Projects/API Projects/Timestamp Microservice/server.js
+++ FCC-Projects/API Projects/Timestamp Microservice/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const PORT = 3000;
 
 
-app.use();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`You are lisining to port ${PORT}`);
+})
