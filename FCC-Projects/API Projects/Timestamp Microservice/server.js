b043e1e2decd95fedcf7ff359ae221f40df0d3b1
tathagata218 <tathagata218@yahoo.com>
2018-02-22 23:46:56 -0600

    this is the stuff

--- FCC-Projects/API Projects/Timestamp Microservice/server.js
+++ FCC-Projects/API Projects/Timestamp Microservice/server.js
@@ -7,12 +7,18 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+
+app.get('/',(req,res)=>{
+res.sendFile('./index.html')
+})
+
 app.get('/:data',(req, res)=>{
     let data = req.params
     
     res.json({
         receivedData : data
     });
+    console.log(data);
 })
 
 
