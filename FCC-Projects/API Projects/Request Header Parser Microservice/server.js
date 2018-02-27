41637f1c84d8af323b89019999f39a30fc220d05
tathagata218 <tathagata218@yahoo.com>
2018-02-26 23:39:32 -0600

    This is Final Update

--- FCC-Projects/API Projects/Request Header Parser Microservice/server.js
+++ FCC-Projects/API Projects/Request Header Parser Microservice/server.js
@@ -10,11 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
 app.get('/',(req,res)=>{
-    console.log(req.headers);
-    console.log(req.headers.connection);
-    console.log(req.headers.location);
-    console.log(req.headers["accept-language"]);
-    console.log(req.headers["user-agent"]);
+    
     const data = {
         ipaddress : req.ip,
         language : req.headers["accept-language"],
