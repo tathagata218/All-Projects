dc2856c54b9a7adca2959f78b10c3cff6726c2b4
tathagata218 <tathagata218@yahoo.com>
2018-02-26 23:05:00 -0600

    This is a update

--- FCC-Projects/API Projects/Request Header Parser Microservice/server.js
+++ FCC-Projects/API Projects/Request Header Parser Microservice/server.js
@@ -2,3 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
+const PORT = 3000;
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+
+
+
