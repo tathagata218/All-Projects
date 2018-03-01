68131dbc64ee6b387d9eee53f99a5d26c07bd5e8
tathagata218 <tathagata218@yahoo.com>
2018-02-28 19:32:26 -0600

    This is an Update

--- FCC-Projects/API Projects/URL Shortener Microservice/server.js
+++ FCC-Projects/API Projects/URL Shortener Microservice/server.js
@@ -8,6 +8,10 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/index.html'));
+    })
+
 app.get(':/urlData',(req,res)=>{
     let clientData = req.params;
 });
