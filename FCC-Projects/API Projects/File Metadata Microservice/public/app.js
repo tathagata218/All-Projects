ae613a9d9578378e6d9fb52506bdbe5452721b8e
tathagata218 <tathagata218@yahoo.com>
2018-03-04 18:25:40 -0600

    This is the stuff

--- FCC-Projects/API Projects/File Metadata Microservice/public/app.js
+++ FCC-Projects/API Projects/File Metadata Microservice/public/app.js
@@ -3,13 +3,7 @@ $(function(){
 $('#submitBtn').click(function(){
     let fileData = 'This will be the file data';
 
-    $.ajax({
-        method : "POST",
-        url : "/fileData",
-        data : fileData
-    }).done(()=>{
-        console.log('Message has been sent');
-    });
+  $.post('/fileData', fileData);
     
     });
 
