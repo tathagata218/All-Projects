fee79f24f9c44d15c4ee257e63cbabdaa7ab135e
tathagata218 <tathagata218@yahoo.com>
2018-03-05 16:58:48 -0600

    This is the stuff

--- FCC-Projects/API Projects/File Metadata Microservice/public/app.js
+++ FCC-Projects/API Projects/File Metadata Microservice/public/app.js
@@ -1,14 +1,17 @@
 $(function(){
 
-$('#file').change(function(){
+$('#file').change(function(e){
+  e.preventDefault();
     let testfile = $('#file')["0"].files['0'].name;
     $('#displayfiles').html(testfile);
 });
 
 
-$('#submitBtn').click(function(){
-
-    let testfile = $('#file')["0"].files;
+$('#submitBtn').click(function(e){
+  e.preventDefault();
+    let test2 = new FormData()
+    test2['file'] =  $('#file')["0"].files["0"];
+    let testfile = $('#file')["0"].files["0"];
     let testObject = {
       fileInfo : $('#file')["0"],
       fileData : $('#file')["0"].files,
@@ -16,12 +19,15 @@ $('#submitBtn').click(function(){
 
     }
     console.log(testObject);
+    console.log(test2);
     
   $.ajax({
-    method : "POST",  
+    type: "POST",  
     URL:'/fileData',
-    data : testfile,
-    contentType : 'multipart/form-data'  
+    processData: false,
+    contentType: false,
+    data : test2,
+    dataType : JSON
   }).done(()=>{
     console.log('Data Has been sent');
   })
