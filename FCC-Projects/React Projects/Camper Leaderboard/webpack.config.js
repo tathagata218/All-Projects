14e7ad8d51dd4e6beb18d99fd13d21cc9880d648
tathagata218 <tathagata218@yahoo.com>
2018-03-08 11:19:33 -0600

    Updated

--- FCC-Projects/React Projects/Camper Leaderboard/webpack.config.js
+++ FCC-Projects/React Projects/Camper Leaderboard/webpack.config.js
@@ -3,6 +3,16 @@ const path = require('path');
 const htmlwebpackplugin = require('html-webpack-plugin');
 const extraTextPlugin = require('extract-text-webpack-plugin');
 
+module.exports = {
+    entry : {
+        app : "./src/app.js"
+    },
+    output : {
+        path : path.resolve(__dirname,'client'),
+        filename : 'js/[name].js'
+    }
+},
+
 
 
 
