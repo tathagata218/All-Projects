6531b256ba8bc6539a476ec65266c4ae6d7d3f11
tathagata218 <tathagata218@yahoo.com>
2018-03-17 17:51:58 -0600

    This is updated

--- Algorithems/FCC-Algorithm/Validate_US_Telephone_Numbers/algo.js
+++ Algorithems/FCC-Algorithm/Validate_US_Telephone_Numbers/algo.js
@@ -1,6 +1,8 @@
-function phoneNumbers(numebr) {
+function phoneNumbers(number) {
     var regEx =/(\(?\d{3}[)-\s]((\s\d{3}[-\s]\d{4})|(\d{3})[-\s]\d{4}))|(\d{10})|(\d\s\d{3}\s\d{3}\s\d{4})/;
+    console.log(regEx.test(number));
     return regEx.test(number)
+  
 }
 
 phoneNumbers('555-555-5555');
