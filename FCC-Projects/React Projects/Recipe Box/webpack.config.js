0976c31527e4a146ffb300a5b92d9cc7b2c30a0f
tathagata218 <tathagata218@yahoo.com>
2018-03-29 16:34:04 -0600

    This is an update

--- FCC-Projects/React Projects/Recipe Box/webpack.config.js
+++ FCC-Projects/React Projects/Recipe Box/webpack.config.js
@@ -1,2 +1,17 @@
 const webpack = require('webpack');
+const path  = require('path');
+
+
+
+module.exports = {
+    entry :{
+        app : "./src/app.js"
+    },
+    output : {
+        path : path.resolve(__dirname, "client"),
+        filename: "js/[name].js"
+    },
+    
+    
+}
 
