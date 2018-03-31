8060dfdfb871a29d260dd71b5de513516e9e80ef
tathagata218 <tathagata218@yahoo.com>
2018-03-31 08:12:29 -0600

    This is updated

--- FCC-Projects/React Projects/Recipe Box/src/main.js
+++ FCC-Projects/React Projects/Recipe Box/src/main.js
@@ -1,10 +1,35 @@
 import React , {Component} from 'react'
 
 
+recepiInfo =[
+    {
+    'Recipe' : "Chicken Curry",
+    "Ingredients" : ["Curry", "Chicken"]
+    }
+]
+
 class App extends Component {
     state = {
 
-    } 
+    }
+    
+    componentWillMount () {
+        if(window.localStorage){
+            localStorage.clear()
+            }
+        else (
+            localStorage.setItem('Recipe',recepiInfo)
+
+        )
+    }
+
+    addRecipe = () =>{
+
+    }
+
+    deleteIngr = () =>{
+
+    }
 
     render () {
         return (
