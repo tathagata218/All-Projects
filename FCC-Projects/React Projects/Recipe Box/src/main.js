b253939b6ab8503059145f7c5cbb9ea4bc7a4835
tathagata218 <tathagata218@yahoo.com>
2018-03-30 20:08:41 -0600

    This is updated

--- FCC-Projects/React Projects/Recipe Box/src/main.js
+++ FCC-Projects/React Projects/Recipe Box/src/main.js
@@ -1,11 +1,15 @@
-import React ,{Component}from 'react'
+import React , {Component} from 'react'
 
 
 class App extends Component {
+    state = {
+
+    } 
+
     render () {
         return (
             <div>
-            This is a test
+            <h1>This is the recipe Box</h1>
             </div>
         )
     }
