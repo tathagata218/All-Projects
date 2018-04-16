ac84e799e9f6d8184e9c132cb718d201b7140f41
tathagata218 <tathagata218@yahoo.com>
2018-04-16 08:29:38 -0600

    This is updated

--- FCC-Projects/API Projects/File Metadata Microservice/server.js
+++ FCC-Projects/API Projects/File Metadata Microservice/server.js
@@ -1,3 +1,11 @@
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
+const stringDecoder = require('string_decoder')
+const decode = new stringDecoder('utf8')
+const PORT = 8080
+
+
+const server = http.createServer((req,res)=>{
+    
+})
