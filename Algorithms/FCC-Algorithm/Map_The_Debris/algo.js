c38d964a48c19219f9e5b66b457d174cbef605a4
tathagata218 <tathagata218@yahoo.com>
2018-04-16 09:44:21 -0600

    This is updated

--- Algorithms/FCC-Algorithm/Map_The_Debris/algo.js
+++ Algorithms/FCC-Algorithm/Map_The_Debris/algo.js
@@ -1,4 +1,8 @@
 function orbitalPeriod(arr) {
+    var avgAlt = arr[0]['avgAlt']
+    var a = Math.pow((avgAlt+earthRadius),3)
+    var result = 2*Math.PI*Math.pow( (a/GM) , 0.5)
+    arr[0].orbitalPeriod = result
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
     return arr;
