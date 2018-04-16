1ff8350bd1be9b34d56625aeaa05fe42a8562abc
tathagata218 <tathagata218@yahoo.com>
2018-04-16 08:37:27 -0600

    This is updated

--- FCC-Projects/API Projects/File Metadata Microservice/server.js
+++ FCC-Projects/API Projects/File Metadata Microservice/server.js
@@ -7,5 +7,32 @@ const PORT = 8080
 
 
 const server = http.createServer((req,res)=>{
-    
+    if (req.method ==='POST' && req.url==='/fileUpload'){
+        const uplodedData = []
+        req.on('data',(data)=>{
+            uplodedData.push(data)
+
+        }).on('end',()=>{
+            res.write(decode.write(uplodedData[0]))
+            res.end()
+
+        })
+    }
+    else {
+        fs.readFile('./index.html', (err,data)=>{
+            res.writeHead(200,{'Content-Type' : 'test/html'});
+            res.write(data)
+            res.end()
+        })
+    }
+})
+
+
+server.listen(PORT, (err)=>{
+    if(!err){
+        console.log(`Connected to port ${PORT}`)
+    }
+    else{
+        console.log(err)
+    }
 })
