8a836df9922e3b89cccb0a96bc2669452f9df74b
Tathagata Das <tathagata218@yahoo.com>
2018-05-02 17:36:04 -0500

    Update algo.js

--- Algorithms/FCC-Algorithm/Inventory_Update/algo.js
+++ Algorithms/FCC-Algorithm/Inventory_Update/algo.js
@@ -1,6 +1,26 @@
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
-    return arr1;
+    var fullArr = arr1.concat(arr2);
+    var resultArr = []; 
+   
+    for(var i = 0; i < fullArr.length; i++){
+        for(var x = i+1; x < fullArr.length - 1 ; x++){
+            
+            if(fullArr[i][1] === fullArr[x][1]){
+                let add = fullArr[i][0] + fullArr[x][0]
+                fullArr.splice(i,1,[add,fullArr[i][1]])
+                fullArr.splice(x,1);
+                
+                
+            }
+           
+        }
+    }
+
+    
+
+
+
+   console.log(fullArr);
 }
 
 // Example inventory lists
